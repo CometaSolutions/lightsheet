159a8247a3efe9e6a44d4eb8447f6d3e10684550
Jaakko Bonifer <jaakko.bonifer@cometasolutions.fi>
2024-05-14 17:05:19 +0300

    Move resolveColumnIndex to helpers

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -15,6 +15,20 @@ export default class LightsheetHelper {
     return label || "A"; // Return "A" if index is 0
   };
 
+  static resolveColumnIndex(column: string): number {
+    let index = 0;
+    for (let i = 0; i < column.length; i++) {
+      if (column[i] < "A" || column[i] > "Z") return -1;
+      let baseIndex = column.charCodeAt(i) - "A".charCodeAt(0);
+      if (i != column.length - 1) baseIndex += 1;
+      // Character index gives us the exponent: for example, 'AAB' is 26^2 * 1 + 26^1 * 1 + 26^0 * 1 + 1
+      const exp = column.length - i - 1;
+      index += baseIndex * Math.pow(26, exp);
+    }
+
+    return index;
+  }
+
   static getKeyPairFor = (cellGroup: CellGroup<ColumnKey | RowKey>, opposingKey: ColumnKey | RowKey): PositionInfo => {
     return {
       columnKey: CellGroup instanceof Column ? cellGroup.key as ColumnKey : opposingKey as ColumnKey,
